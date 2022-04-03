aa7817bd60425fc9f5abed17a9be0debb53069c0
Surya <123004250@sastra.ac.in>
2022-04-03 09:01:22 +0530

    feat: added card menu component

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,4 +1,4 @@
-import { Home, Liked, History, PlayList, WatchLater } from '../src/pages/index';
+import { Home, Liked, History, PlayList, WatchLater, VideoPlayerPage } from '../src/pages/index';
 import { Route, Routes } from 'react-router-dom';
 import { Signup } from './pages/Authentication/Signup';
 import { Login } from './pages/Authentication/Login';
@@ -7,12 +7,13 @@ export const AppRoutes = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path=':videoId' element={<VideoPlayerPage />} />
       <Route path='/liked' element={<Liked />} />
       <Route path='/history' element={<History />} />
       <Route path='/playList' element={<PlayList />} />
       <Route path='/watchlater' element={<WatchLater />} />
-      <Route path='/signup' element={<Signup/>}/>
-      <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/login' element={<Login />} />
       <Route path='*' element={
         <main style={{ padding: "1rem" }}>
           <p>There's nothing here!</p>
