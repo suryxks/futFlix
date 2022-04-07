85a72dba85bdc8bcbd312bda4ab7d534b962c5db
Surya <123004250@sastra.ac.in>
2022-04-07 10:24:52 +0530

    added routes and styling for app

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ import { Header } from "./Header/Header";
 import { Sidebar } from "./sidebar/Sidebar";
 import { VideoCard } from "./videoCard/videoCard";
 import { PlaylistCard } from "./PlaylistCard/PlaylistCard";
-export { Header, Sidebar, VideoCard, PlaylistCard }
+import { PlaylistModal } from "./PlayListModal/PlayListModal";
+export { Header, Sidebar, VideoCard, PlaylistCard,PlaylistModal }
